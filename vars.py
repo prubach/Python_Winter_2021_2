b173337bfe7a38c895ce430555b53f1589186802
Pawel Rubach <pawel.rubach@sgh.waw.pl>
2021-11-03 15:56:34 +0100

    Variables - floats, ints etc.

--- vars.py
+++ vars.py
@@ -11,3 +11,37 @@ if __name__ == '__main__':
     cf = af + bf
     print(type(cf))
     print('cf:{}'.format(cf))
+
+    print(1.79e+308)
+    print(1.8e+308)
+
+    bb = 0b11111111
+    print(bb)
+
+    bx = 0xff
+    print(bx)
+
+    print(bb/bx)
+
+    f = 5
+
+    print(f**3)
+    g = 12
+    print('div g: {}'.format(g % 5))
+
+    h = 'ff'
+    print(int(h, 16))
+    h1 = '563463'
+    h1i = int(h1)
+    print(h1.isdigit())
+    print(h1.isalpha())
+    print(h1.isalnum())
+
+    print(type(h1))
+    print(type(h1i))
+
+    fa = 100
+    print(type(fa))
+    fa = 100.3563
+    print(type(fa))
+    print(round(fa, 2))
