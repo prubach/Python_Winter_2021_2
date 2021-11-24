5aed9320655fa4af87716ace711c2ea4ab6e7e21
Pawel Rubach <pawel.rubach@sgh.waw.pl>
2021-11-24 16:13:57 +0100

    lists 2D

--- lists_2d.py
+++ lists_2d.py
@@ -1,2 +1,39 @@
-l2d = [[4, 7, 8], [6, 4, 8], [3, 5, 8], [2, 7, 8]]
+l2d = [[4, 7, 8],
+       [6, 4, 8],
+       [3, 5, 8],
+       [2, 7, 8]]
 print(l2d[2][0])
+# TODO
+# sum rows
+# sum cols
+# sum all elements
+sums = []
+for el in l2d:
+    sums.append(sum(el))
+print(sums)
+
+print('sum all based on sum rows: {}'.format(sum(sums)))
+
+
+#sums_cols = [0, 0, 0]
+sums_cols = []
+#sums_cols = [0, 0, 0]
+for el in l2d:
+    for i in range(len(el)):
+        if len(sums_cols) > i:
+            sums_cols[i] += el[i]
+        else:
+            sums_cols.append(el[i])
+print(sums_cols)
+
+print('sum all based on sum cols: {}'.format(sum(sums)))
+
+#sums2 = [sum(el) for el in l2d if len(el) > 2]
+#sums2 = [sum(el) for el in l2d]
+#print(sums2)
+
+import numpy as np
+
+print(np.sum(l2d, axis=0))
+print(np.sum(l2d, axis=1))
+print(np.sum(l2d))
