1b014bae6cbb096026319930942821d4b91ff28b
Pawel Rubach <pawel.rubach@sgh.waw.pl>
2021-11-24 16:24:15 +0100

    JSON pickling

--- file_write_json.py
+++ file_write_json.py
@@ -1,12 +1,12 @@
-import _pickle
+import json
 d1 = {'a': 525, 'a': 56464, 'bb': 56464, 'ccc': 3747, 'ddd': [25356, 474, 6346, 'wwhwrh']}
 #d1['ddd'] = 436536
 
-with open('data/out_d.dat', 'wb') as f:
-    _pickle.dump(d1, f)
+with open('data/out_d.json', 'w') as f:
+    json.dump(d1, f)
 
 
-with open('data/out_d.dat', 'rb') as f:
-    out_d = _pickle.load(f)
+with open('data/out_d.json', 'r') as f:
+    out_d = json.load(f)
     print(out_d)
     print(type(out_d))
