4e7f38755655671603ea3a9f8725b14036906adc
Pawel Rubach <pawel.rubach@sgh.waw.pl>
2021-12-01 16:04:23 +0100

    Fixed folder space summing up...

--- folders.py
+++ folders.py
@@ -1,14 +1,19 @@
 import os.path
 
-print(__file__)
-python_folder = os.path.dirname(__file__)
+#print(__file__)
+#python_folder = os.path.dirname(__file__)
+python_folder = 'C:/Temp'
 print(python_folder)
 
 sum_bytes = 0
 for file in os.listdir(python_folder):
-    if os.path.isfile(file):
-        print(file)
+    f = os.path.join(python_folder, file)
+    if os.path.isfile(f):
+        #print(file)
         #print(os.stat(file))
-        sum_bytes += os.path.getsize(file)
-        print(os.path.getsize(file))
+        sum_bytes += os.path.getsize(f)
+        #print(os.path.getsize(file))
 print('Sum space: {}'.format(sum_bytes))
+
+# 1. Include summing space occupied for folders with their content
+# Hint: use recursion...
