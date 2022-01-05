574013462d4102070b643b90499b899ea61c5bc5
Paweł Rubach <pawel.rubach@gmail.com>
2022-01-05 16:11:06 +0100

    Bank structure - account

--- bank.py
+++ bank.py
@@ -13,3 +13,28 @@ class Customer:
         return 'Customer[{},{},{},{}]'.format(self.id, self.first_name, self.last_name, self.email)
 
 
+class Account:
+    last_id = 0
+
+    def __init__(self, customer):
+        Account.last_id += 1
+        self.id = Account.last_id
+        self.customer = customer
+        self._balance = 0
+
+    def __repr__(self):
+        return 'Account[{},{},{}]'.format(self.id, self.customer.last_name, self._balance)
+
+
+class SavingsAccount(Account):
+    interest_rate = 0.02
+
+    def calc_interest(self):
+        self._balance += self.interest_rate * self._balance
+
+
+class CheckingAccount(Account):
+    pass
+
+
+
