175374ca699bfffcbc8a2052a1fa84306fa6e289
Paweł Rubach <pawel.rubach@gmail.com>
2022-01-05 16:06:00 +0100

    Bank structure - customer

---
+++ bank.py
@@ -0,0 +1,15 @@
+
+class Customer:
+    last_id = 0
+
+    def __init__(self, first_name, last_name, email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        Customer.last_id += 1
+        self.id = Customer.last_id
+
+    def __repr__(self):
+        return 'Customer[{},{},{},{}]'.format(self.id, self.first_name, self.last_name, self.email)
+
+
